e8a24ee461e9b724b9d72958ca2f042fbdb76b9c
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-05-07 22:08:39 +0100

    changed the sanityclient to client3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b70561c0f192dcf4"; }
+/******/ 		__webpack_require__.h = function() { return "04f6026074787681"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,5 +1,5 @@
 import {createClient} from "next-sanity";
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'zr0owxdx';
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
